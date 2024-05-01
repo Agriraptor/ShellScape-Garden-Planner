5f27574e324f6b4ed703ffef84b711332a27df77
michaelma7 <michaelma16688@gmail.com>
2024-05-01 16:08:01 -0400

    fixed server and implemented bcrypt

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,21 +6,18 @@ const userRouter = express.Router();
 // EXPECT THE FOLLOWING FOR SIGNUP LOGIN
 // baseurl.com/user/signup
 // { username: 'username', password: 'password' }
-userRouter.post(
-  '/signup',
-  userController.checkUser,
-  userController.createUser,
-  (req, res) => {
-    //redirect to creategarden page (frontend)
-    res.status(200).json(res.locals.message);
-  }
-);
+userRouter.post('/signup', userController.createUser, (req, res) => {
+  //if user was created successfully return 200 status else have frontend redirect
+  if (res.locals.userCreated) res.sendStatus(201);
+  else res.sendStatus(417);
+});
 
 //add a route for login
 // baseurl.com/user/login
 userRouter.post('/login', userController.loginUser, (req, res) => {
   //check if username and password exist on db if not return to sign up page
-  res.status(200).json(res.locals.message);
+  if (res.locals.user) res.sendStatus(200);
+  else res.sendStatus(417);
 });
 
 //update password
